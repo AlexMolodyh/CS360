145fb382fb2af9611ebc7e2a4aaa580483618ca6
Alex Molodyh <amolodyh16@mail.wou.edu>
2017-11-16 10:28:13 -0800

    working on lab4

--- Week8/haskellTutorial1/functions.hs
+++ Week8/haskellTutorial1/functions.hs
@@ -18,3 +18,22 @@ strlength st = length [s | s <- st]
 makeList f = [[x | x <- [1..s]] | s <- [1..f] ]
 
 sanitize st = concat [if c `elem` [' '] then ['%','2','0'] else (c:[]) | c <- st]
+
+lucky :: (Integral a) => a -> String
+lucky 7 = "LUCKY NUMBER SEVEN"
+lucky 3 = "THIRD TIME'S THE CHARM!!"
+lucky x = "Sorry, you're out of luck, pal!"
+
+func1 :: String -> Int
+func1 "Hello" = 1
+func1 xs = length [x | x <- xs]
+
+--recursive factorial
+factorial :: (Integral a) => a -> a
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
+
+--bilds weird matrix
+bList x = [ [ [ t | t <- [1..s]] | s <- [1..x]] | x <- [1..20]]
+
+multMatrix b c = [b * c | b <- [1..b], c <- [1..c]]
